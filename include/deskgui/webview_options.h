4139cc7b31906adbdfadf36d33c742be9725a8c2
Marc Ortuño <marc.ortuno@voicemod.net>
2024-03-27 15:20:43 +0100

    Create specific user data folder in Windows webview2

--- include/deskgui/webview_options.h
+++ include/deskgui/webview_options.h
@@ -13,16 +13,43 @@
 #include <variant>
 
 namespace deskgui {
+  /**
+   * @brief Class representing the options for a webview.
+   * 
+   * Note: In Windows, all webviews should share the same WebviewOptions. 
+   * https://github.com/MicrosoftEdge/WebView2Feedback/issues/2323#issuecomment-1088248488
+   * 
+   */
   class WebviewOptions {
   public:
     using Option = std::variant<bool, int>;
 
+    /**
+     * @brief Set an option with the specified key and value.
+     * 
+     * @param key The key of the option.
+     * @param value The value of the option.
+     */
     inline void setOption(const std::string& key, const Option& value) { options[key] = value; }
 
+    /**
+     * @brief Check if an option with the specified key exists.
+     * 
+     * @param key The key of the option.
+     * @return true if the option exists, false otherwise.
+     */
     inline bool hasOption(const std::string& key) const {
       return options.find(key) != options.end();
     }
 
+    /**
+     * @brief Get the value of an option with the specified key.
+     * 
+     * @tparam T The type of the option value.
+     * @param key The key of the option.
+     * @return The value of the option.
+     * @throws std::runtime_error if the option value is not of the requested type.
+     */
     template <typename T> T getOption(const std::string& key) const {
       auto it = options.find(key);
       if (it != options.end()) {
